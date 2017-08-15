dd758bd8b23c00ca761a531c3f3f26832c69b2cf
poligabi <polianagasl@gmail.com>
2017-08-15 08:25:07 -0300

    Update Ud-ggplot .R

--- Ud-ggplot .R
+++ Ud-ggplot .R
@@ -1,7 +1,20 @@
 ####################################################################
 # Udemy - R,ggplot and Simples Linear Regression - Charles Redmond #
 ####################################################################
+library (ggplot2)
 
+## Tipos de graficos:
+
+ggplot()+geom_point(data=,aes(x=,y=),size=,color="",shape=)
+
+ggplot()+geom_line(data= data frame ,aes(x=,y=))
+
+ggplot(data=, aes(x=factor(), y =, fill =factor()))+ 
+  geom_bar(stat = "identity", colour="black", position = position_dodge(0.9)) +
+  geom_errorbar(limits, position = position_dodge(0.9), width = 0.25)
+
+## R colors: http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
+## R shapes: http://sape.inf.usi.ch/quick-reference/ggplot2/shape
 
 
 
